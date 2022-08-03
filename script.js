48660a759d9ee958f39d2c268d98b6a7b7bf76d6
Muhazamali <muhazam.ali49@gmail.com>
2022-08-03 07:03:04 -0700

    dynamic tabs with js

---
+++ script.js
@@ -0,0 +1,22 @@
+
+function txt1(a){
+    document.body.style.backgroundColor="gray";
+    document.getElementById("hdtxt").innerHTML = "Communication";
+    document.getElementById("ptxt").innerHTML = "Communication is the act of giving, receiving, and sharing information -- in other words, talking or writing, and listening or reading. Good communicators listen carefully, speak or write clearly, and respect different opinions.";
+    document.getElementById("txtbox").style.backgroundColor="green";
+    document.getElementById("btn-1").style="background-color:green;padding:25px;border:none";
+    document.getElementById("btn-2").style="background-color:orange;padding:25px;border:none";
+    document.getElementById("btn-3").style="background-color:red;padding:25px;border:none";
+    document.getElementById("ptxt").style="font-size:1.5rem;color:white;padding:40px;";
+    document.getElementById("hdtxt").style="font-size:2.8rem;color:white;padding:25px;text-align:center;";
+}
+function txt2(){
+    document.getElementById("hdtxt").innerHTML = "Purpose of communication";
+    document.getElementById("ptxt").innerHTML = "Communication serves five major purposes: to inform, to express feelings, to imagine, to influence, and to meet social expectations. Each of these purposes is reflected in a form of communication.";
+    document.getElementById("txtbox").style.backgroundColor="orange";
+}
+function txt3(){
+    document.getElementById("hdtxt").innerHTML = "Importance of communication";
+    document.getElementById("ptxt").innerHTML = "How important is communicationIn our daily life, communication helps us build relationships by allowing us to share our experiences, and needs, and helps us connect to others. It's the essence of life, allowing us to express feelings, pass on information and share thoughts. We all need to communicate.";
+    document.getElementById("txtbox").style.backgroundColor="red";
+}
